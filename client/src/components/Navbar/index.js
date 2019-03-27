022c107954940b9aca21c7306fc01871cc3f6591
Nina Withers <ninarosewithers@gmail.com>
2019-03-26 22:35:18 -0400

    adding more changes to nav

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -7,44 +7,51 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import { Link as RouterLink } from 'react-router-dom'
+import Link from '@material-ui/core/Link';
 
 const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
+    root: {
+        flexGrow: 1,
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
 };
 
 function Navbar(props) {
-  const { classes } = props;
-  return (
-    <div className={classes.root}>
-      <AppBar position="static"  style={{ background: 'transparent', boxShadow: 'none'}}>
-        <Toolbar>
-          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" className={classes.grow}>
-          <Button color="inherit">About</Button>
-          <Button color="inherit">FAQ</Button>
-          </Typography>
-          {/* <Button color="inherit">About</Button>
-          <Button color="inherit">FAQ</Button> */}
-          <Button color="inherit">Login / Sign up</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+            <AppBar position="static" style={{ background: 'transparent', boxShadow: 'none' }}>
+                <Toolbar>
+                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h4" color="inherit" className={classes.grow}>
+                        BPVA
+                    </Typography>
+                    <Link component={RouterLink} to="/">
+                        <Button size="large" color="inherit">About</Button>
+                    </Link>
+                    <Link component={RouterLink} to="/login">
+                        <Button size="large" color="inherit">Login / Sign up</Button>
+                    </Link>
+                    <Link component={RouterLink} to="/">
+                        <Button size="large" color="inherit">FAQ</Button>
+                    </Link>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
 
 Navbar.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Navbar);
