02f7625f4ae3093338c5b2ea129e9f2a78a9bbc9
Kirthi Bisset <kirthi.bisset@yahoo.com>
2019-03-28 09:34:06 -0400

    video player working

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
-// import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player'
 // import YouTubePlayer from 'react-player/lib/players/YouTube'
 
 
 
 class Video extends Component {
   render () {
-      return <h2 style={{ display: 'flex', justifyContent: 'center', marginBottom: 20 }}>Video Player</h2>
-    // return <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' playing />
-  }
+    return (
+    <div className='player-wrapper'>
+      <ReactPlayer
+        className='react-player'
+        url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+      />
+    </div>
+  )
+}
 }
 
 export default Video;
