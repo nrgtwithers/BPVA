ca3d824849d44c1e9c899e35057338e39d77152d
Kirthi Bisset <kirthi.bisset@yahoo.com>
2019-03-29 13:12:24 -0400

    animation

---
+++ client/src/components/TextEffect/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './style.css';
+
+export default function TextEffect() {
+    return (
+        <div>
+            <ul class="Words">
+                <li class="Words-line">
+                    <p>&nbsp;</p>
+                    <p>BPVA</p>
+                </li>
+                <li class="Words-line">
+                    <p>BPVA</p>
+                    <p>Beauty</p>
+                </li>
+                <li class="Words-line">
+                    <p>Beauty</p>
+                    <p>Professional</p>
+                </li>
+                <li class="Words-line">
+                    <p>Professional</p>
+                    <p>Virtual</p>
+                </li>
+                <li class="Words-line">
+                    <p>Virtual</p>
+                    <p>Assistant</p>
+                </li>
+                <li class="Words-line">
+                    <p>Assistant</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
 import FAQ from '../components/Collapsable';
 import Video from "../components/Video";
 import { Fab } from '@material-ui/core';
+import TextEffect from '../components/TextEffect';
 
 
 class Home extends Component {
@@ -23,6 +24,7 @@ class Home extends Component {
 
                 <h1 style={{ display: 'flex', justifyContent: 'center', marginBottom: 0 }}>BPVA</h1>
                 <p style={{ display: 'flex', justifyContent: 'center', marginTop: 5 }}>Beauty Professional Virtual Assistant</p>
+                <TextEffect/>
                 <Video />
                 <FAQ />
 
